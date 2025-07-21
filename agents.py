6cd7006ecd35f8b4b33930dc2fcd3e8dde9e74df
Idan Angel <idanangel@users.noreply.github.com>
2025-07-21 11:18:48 +0300

    Add open API data source support

--- agents.py
+++ agents.py
@@ -1,4 +1,5 @@
 
+import requests
 from db import PostgresDB
 from pydantic_ai import Agent
 from instrument import logfire
@@ -22,6 +23,28 @@ def get_db_schema_and_sample_data(run_context):
         full_context_for_llm = schema_info + sample_data
 
     return full_context_for_llm
+
+@agent.tool
+def get_api_schema(run_context):
+    """Get the Open API schema."""
+    response = requests.get("http://0.0.0.0:8001/openapi.json")
+    return response.text
+
+@agent.tool_plain
+def run_api_query(endpoint: str, method: str = "GET", data: dict = None) -> str:
+    """
+    Run a query against the API and return the results.
+    Args:
+        endpoint: The endpoint to query.
+        method: The HTTP method to use (GET, POST, PUT, DELETE, etc.).
+        data: The data to send with the request (for POST, PUT).
+    """
+    try:
+        response = requests.request(method, f"http://0.0.0.0:8001/{endpoint}", json=data)
+        return response.text
+    except Exception as e:
+        return f"Error executing query: {e}."
+
 
 @agent.tool_plain
 def run_sql_query(query: str) -> str:
